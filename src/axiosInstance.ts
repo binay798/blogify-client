8c0035e62e3e74ba902965ef8c15a02d3f781cb1
Binay shrestha <binayshrestha798@gmail.com>
2021-12-14 19:04:14 +0545

    changed baseurl for production app

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:8000';
+export let baseUrl = 'http://localhost:8000';
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = 'https://blogify-node-app.herokuapp.com/';
+}
 const axiosInstance = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
